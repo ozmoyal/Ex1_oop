136c3b7c8b07169b6908d5af012b903adbd270f8
shirelIsr <shirel.isr@gmail.com>
2019-12-02 16:47:25 +0200

    02.12.19 -16:47

--- src/Ex1/ComplexFunction.java
+++ src/Ex1/ComplexFunction.java
@@ -10,13 +10,35 @@ public class ComplexFunction implements complex_function {
 		this.left=null;
 		this.op=Operation.Error;
 	}
-	public ComplexFunction(function l,function r,Operation o)
+	public ComplexFunction(function l,function r,String o)
 	{
 		this.left=l;
 		this.right=r;
-		this.op=o;
+		o.toLowerCase();
+		switch(o)
+		{
+		case ("plus"):
+			this.op=Operation.Plus;
+		case ("divid"):
+			this.op=Operation.Divid;
+		case ("times"):
+			this.op=Operation.Times;
+		case ("nune"):
+			this.op=Operation.None;
+		case ("error"):
+			this.op=Operation.Error;
+		case ("comp"):
+			this.op=Operation.Comp;
+		case ("max"):
+			this.op=Operation.Max;
+		case ("min"):
+			this.op=Operation.Min;
+		default: 
+			this.op=Operation.Error;
+
+		}
 	}
-	public ComplexFunction(function l)
+	public ComplexFunction(function l,String o)
 	{
 		this.left=l;
 		this.right=null;
@@ -25,57 +47,86 @@ public class ComplexFunction implements complex_function {
 
 	@Override
 	public double f(double x) {
-		
+		return 0;
+
 	}
 
 	@Override
 	public function initFromString(String s) {
+
 		s=s.toLowerCase();
-		
-		
+		function left=new Polynom();
+		String op="";
+		if(s.contains("("))
+		{
+			s.charAt(s.length()-1);
+			if(s.charAt(s.length())==')')
+			{
+				int index =s.indexOf("(");
+				op=s.substring(0,index-1);
+				s=s.substring(index+1,s.length()-2);
+				left=this.initFromString(s);
+			}
+			if(s.contains(","))
+			{
+				int index=s.indexOf(",");
+				function right=new Polynom(s.substring(index+1,s.length()));
+				left=new Polynom(s.substring(0,index-1));
+				return new ComplexFunction(left,right,op);
+
+
+			}
+			left=new Polynom(s);
+			return new ComplexFunction(left,op);
+
+
+
+
+		}
+
 		return null;
-		
+
 	}
 
 	@Override
 	public function copy() {
-		return new ComplexFunction(this.left,this.right,this.op);
+		return new ComplexFunction(this.left,this.right,this.op.toString());
 	}
 
 	@Override
 	public void plus(function f1) {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 	@Override
 	public void mul(function f1) {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 	@Override
 	public void div(function f1) {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 	@Override
 	public void max(function f1) {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 	@Override
 	public void min(function f1) {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 	@Override
 	public void comp(function f1) {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 	@Override
@@ -85,7 +136,7 @@ public class ComplexFunction implements complex_function {
 
 	@Override
 	public function right() {
-	
+
 		return this.right;
 	}
 
